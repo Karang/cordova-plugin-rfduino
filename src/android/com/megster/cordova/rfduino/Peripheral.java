e1c8fee38da317cb4f54f0a293cfef0a687efb39
root <arthur.hennequin@free.fr>
2015-02-01 04:27:09 +0100

    Fix name=undefined on android

--- src/android/com/megster/cordova/rfduino/Peripheral.java
+++ src/android/com/megster/cordova/rfduino/Peripheral.java
@@ -32,6 +32,11 @@ import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.UUID;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Peripheral wraps the BluetoothDevice and provides methods to convert to JSON.
  */
@@ -59,11 +64,17 @@ public class Peripheral extends BluetoothGattCallback {
     private BluetoothGattCharacteristic sendCharacteristic;
     private BluetoothGattCharacteristic disconnectCharacteristic;
 
+	private String name;
+	
     public Peripheral(BluetoothDevice device, int advertisingRSSI, byte[] scanRecord) {
 
         this.device = device;
         this.advertisingRSSI = advertisingRSSI;
         this.advertising = getAdvertisingValue(scanRecord);
+		name = device.getName();
+		if (name == null) {
+			name = parseAdertisedData(scanRecord);
+		}
 
     }
 
@@ -77,7 +88,7 @@ public class Peripheral extends BluetoothGattCallback {
         JSONObject json = new JSONObject();
 
         try {
-            json.put("name", device.getName());
+            json.put("name", this.name);
             json.put("uuid", device.getAddress()); // This is MAC address
             json.put("advertising", advertising );
             // TODO real RSSI if we have it, else
@@ -205,6 +216,54 @@ public class Peripheral extends BluetoothGattCallback {
         }
         return advertising;
     }
+	
+	private String parseAdertisedData(byte[] advertisedData) {      
+		List<UUID> uuids = new ArrayList<UUID>();
+		String name = null;
+		if( advertisedData == null ){
+			return "NULL";
+		}
+
+		ByteBuffer buffer = ByteBuffer.wrap(advertisedData).order(ByteOrder.LITTLE_ENDIAN);
+		while (buffer.remaining() > 2) {
+			byte length = buffer.get();
+			if (length == 0) break;
+
+			byte type = buffer.get();
+			switch (type) {
+				case 0x02: // Partial list of 16-bit UUIDs
+				case 0x03: // Complete list of 16-bit UUIDs
+					while (length >= 2) {
+						uuids.add(UUID.fromString(String.format(
+								"%08x-0000-1000-8000-00805f9b34fb", buffer.getShort())));
+						length -= 2;
+					}
+					break;
+				case 0x06: // Partial list of 128-bit UUIDs
+				case 0x07: // Complete list of 128-bit UUIDs
+					while (length >= 16) {
+						long lsb = buffer.getLong();
+						long msb = buffer.getLong();
+						uuids.add(new UUID(msb, lsb));
+						length -= 16;
+					 }
+					break;
+				case 0x09:
+					byte[] nameBytes = new byte[length-1];
+					buffer.get(nameBytes);
+					try {
+						name = new String(nameBytes, "utf-8");
+					} catch (UnsupportedEncodingException e) {
+						e.printStackTrace();
+					}
+					break;
+				default:
+					buffer.position(buffer.position() + length - 1);
+					break;
+				}
+			}
+		return name;
+	}
 
     public void updateRssi(int rssi) {
         advertisingRSSI = rssi;
